55cd82f6a394ad8e88b91e11cb90ecd66de3899b
Renzibuguis <zonpan94@gmail.com>
2016-05-16 16:13:48 -0500

    Nuevo dashboard

--- public/app.js
+++ public/app.js
@@ -10,9 +10,7 @@
     function config($routeProvider, $locationProvider) {
         $routeProvider
             .when('/home', {
-                controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
-                controllerAs: 'vm'
+                templateUrl: 'home/dash.html'
             })
 
             .when('/login', {
--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
     development: {
-        phrase: 'holi',
+        phrase: 'estefany',
         rootPath: rootPath,
         db: 'mongodb://admin:admin@ds021711.mlab.com:21711/pizzaplaneta'/*'mongodb://localhost/pizzaplaneta'*/,
         port: process.env.PORT || 9001
