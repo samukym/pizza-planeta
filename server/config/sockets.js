3d83513939c37bc106c75ffc359ac6422f932b0e
Estefany Valdivieso <evaldivieso@ITLAB-Mac-3.local>
2016-06-30 11:43:42 -0500

    avance configuración de sockets

--- server/config/sockets.js
+++ server/config/sockets.js
@@ -1,10 +1,27 @@
+var controllers = require('../controllers');
+
 module.exports = function(app) {
   var io = require('socket.io')(app);
 
-  io.on('connection', function (socket) {
-    socket.emit('mensajito', { mensaje: 'holi' });
-    socket.on('otro evento', function (data) {
+  io.on('connection', function(socket) {
+    //TODO: Probar si funciona la conexión con el cliente iOs
+    console.log('someone connected');
+    socket.emit('mensajito', {
+      mensaje: 'holi'
+    });
+    socket.on('otro evento', function(data) {
       console.log(data);
     });
+
+    //TODO
+    socket.on('actualizar ubicacion', function(data) {
+      console.log(data);
+
+    });
+    //TODO
+    socket.on('disconnect', function() {
+
+    });
   });
+
 }
