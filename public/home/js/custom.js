7d8aac9e74376018cc61d3212f69910fe1d7a74a
Renzibuguis <zonpan94@gmail.com>
2016-07-06 13:40:13 -0500

    QR en Dashboard

--- public/home/js/custom.js
+++ public/home/js/custom.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
                 $SIDEBAR_MENU.find('li').removeClass('active');
                 $SIDEBAR_MENU.find('li ul').slideUp();
             }
-            
+
             $li.addClass('active');
 
             $('ul:first', $li).slideDown(function() {
@@ -83,9 +83,9 @@ $(document).ready(function() {
     }).parent().addClass('active');
 
     // recompute content when resizing
-    $(window).smartresize(function(){  
-        setContentHeight();
-    });
+    // $(window).smartresize(function(){
+    //     setContentHeight();
+    // });
 
     // fixed sidebar
     if ($.fn.mCustomScrollbar) {
@@ -104,15 +104,15 @@ $(document).ready(function() {
         var $BOX_PANEL = $(this).closest('.x_panel'),
             $ICON = $(this).find('i'),
             $BOX_CONTENT = $BOX_PANEL.find('.x_content');
-        
+
         // fix for some div with hardcoded fix class
         if ($BOX_PANEL.attr('style')) {
             $BOX_CONTENT.slideToggle(200, function(){
                 $BOX_PANEL.removeAttr('style');
             });
         } else {
-            $BOX_CONTENT.slideToggle(200); 
-            $BOX_PANEL.css('height', 'auto');  
+            $BOX_CONTENT.slideToggle(200);
+            $BOX_PANEL.css('height', 'auto');
         }
 
         $ICON.toggleClass('fa-chevron-up fa-chevron-down');
@@ -246,36 +246,36 @@ if (typeof NProgress != 'undefined') {
 
 /**
  * Resize function without multiple trigger
- * 
+ *
  * Usage:
- * $(window).smartresize(function(){  
+ * $(window).smartresize(function(){
  *     // code here
  * });
  */
-(function($,sr){
-    // debouncing function from John Hann
-    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-    var debounce = function (func, threshold, execAsap) {
-      var timeout;
-
-        return function debounced () {
-            var obj = this, args = arguments;
-            function delayed () {
-                if (!execAsap)
-                    func.apply(obj, args);
-                timeout = null; 
-            }
-
-            if (timeout)
-                clearTimeout(timeout);
-            else if (execAsap)
-                func.apply(obj, args);
-
-            timeout = setTimeout(delayed, threshold || 100); 
-        };
-    };
-
-    // smartresize 
-    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-})(jQuery,'smartresize');
+// (function($,sr){
+//     // debouncing function from John Hann
+//     // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+//     var debounce = function (func, threshold, execAsap) {
+//       var timeout;
+//
+//         return function debounced () {
+//             var obj = this, args = arguments;
+//             function delayed () {
+//                 if (!execAsap)
+//                     func.apply(obj, args);
+//                 timeout = null;
+//             }
+//
+//             if (timeout)
+//                 clearTimeout(timeout);
+//             else if (execAsap)
+//                 func.apply(obj, args);
+//
+//             timeout = setTimeout(delayed, threshold || 100);
+//         };
+//     };
+//
+//     // smartresize
+//     jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+//
+// })(jQuery,'smartresize');
