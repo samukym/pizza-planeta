350fc689fa9347e8839235ebb231dd84f3c2b3a2
samukym <martinez.95a@gmail.com>
2016-07-06 17:05:28 -0500

    askjdf

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
         phrase: 'frasesecreta',
         rootPath: rootPath,
-        db: 'mongodb://localhost/pizzaplaneta',
+        db: 'mongodb://admin:admin@ds021711.mlab.com:21711/pizzaplaneta',
         googleApiKey: 'AIzaSyATf8VCiWtO-CxqJQI9HynuU82L4RISoNs',
         port: process.env.PORT || 9001
     },
